e83bb696d5f0a6a44dc88dd7341871371761a7c2
aryankeluskar <aryankeluskar6@gmail.com>
2024-05-03 23:22:19 -0700

    again bugfix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,9 @@
-import Link from "next/link";
 
 export default function HomePage() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
       <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-          This is part of theo's reactjs tutorial lol, do not judge. also if you are reading this then thanks for stalking me and do consider following me on github and instagram!
+          This is part of theo reactjs tutorial lol, do not judge. also if you are reading this then thanks for stalking me and do consider following me on github and instagram!
             testing again again again nope pls pls pls pls pls
       </div>
     </main>
