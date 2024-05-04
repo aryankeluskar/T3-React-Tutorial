206d8346aff931f9059a687db4ed4972f636ab07
aryankeluskar <ary.rpi6@gmail.com>
2024-05-03 23:15:26 -0700

    again

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function HomePage() {
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
       <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
           This is part of theo's reactjs tutorial lol, do not judge. also if you are reading this then thanks for stalking me and do consider following me on github and instagram!
-            testing again again again nope pls pls
+            testing again again again nope pls pls pls
       </div>
     </main>
   );
