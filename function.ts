48c91b5e80cf8f400bb3165f26e55bbe264f432a
aryankeluskar <aryankeluskar6@gmail.com>
2024-05-26 04:44:57 +0530

    functions

---
+++ function.ts
@@ -0,0 +1,30 @@
+// FUNCTIONS
+
+// since i did not specify the type of the argument, TS will infer the type to be ANY which is problematic
+// therefore, unlike variables, it is important to specify the type of the arguments in functions
+function addTwo(num: number): number{
+    // return "Hello"; - throws an error because the function is supposed to return a number
+    return num + 2;
+}
+
+console.log(addTwo(2));
+
+
+// good practice to specify the return type of void function so other devs know that the function does not return anything
+function login(name: string, password: string, age: number = 20): void{
+    console.log(name + " has logged in at age " + age);
+}
+
+login("Aryan", "password", 18);
+login("Aryan", "password");
+// login(); - throws an error because the function requires two arguments instead of assuming undefined in normal JS
+
+let inputUser = (name: string, email: string): string => {
+    console.log(name + " has email " + email);
+    return name + " has email " + email;
+}
+
+// The never type represents values which are never observed. In a return type, this means that the function throws an exception or terminates execution of the program.
+function throwError(message: string): never{
+    throw new Error(message);
+}
--- intro.ts
+++ intro.ts
@@ -1,9 +1,22 @@
+// TYPES 
 let user = { name: "Aryan", age: 18 };
 
 console.log("Aryan");
 console.log(user.name);
 
 let email : string = "hi@aryan.greenflag"
+// this colon is not required because TS can infer the type based on the value assigned to the variable, but it is good practice to specify the type of the variable.
 
 console.log(email);
+
+email.toLocaleUpperCase();
+// user.toLocaleUpperCase();
+
+// as you can see, a squiggly line appears under the user object, this is because the user object is not a string and the toLocaleUpperCase() method is not available for objects.
+
+let loggedIn : boolean = true;
+
+let fun : any = "Hello";
+fun = 20;
+console.log(fun);
 
